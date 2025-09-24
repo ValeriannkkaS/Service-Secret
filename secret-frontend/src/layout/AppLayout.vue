789ba159329f77435eafe66879abacbe04d537e5
ValeriankaS <timonkin.max@yandex.com>
2025-09-24 12:56:51 +0300

    базовая начальная настройка фронта - роутер и базовый layout

---
+++ secret-frontend/src/layout/AppLayout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="background">
+    <div class="form-secret-phrase-container"></div>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  width: 100%;
+  height: 100%;
+  background: #d6cccc;
+}
+.form-secret-phrase-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 800px;
+  min-height: 500px;
+  width: 50vw;
+  height: 50vh;
+  margin: 0 auto;
+  transform: translateY(50%);
+  background: #ebebeb;
+  border-radius: 30px;
+  box-shadow: 5px 5px 10px rgba(121, 121, 121, 0.63);
+}
+</style>
--- secret-frontend/src/router/index.ts
+++ secret-frontend/src/router/index.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import SecretForm from '@/components/forms/SecretForm.vue'
+import SecretPhraseResponse from '@/components/SecretPhraseResponse.vue'
+
+const routes = [
+  { path: '/secret', component: SecretForm },
+  { path: '/secret/:link', component: SecretPhraseResponse },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes,
 })
 
 export default router
