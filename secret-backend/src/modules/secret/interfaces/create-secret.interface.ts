8c942befe0aee1f38b2fdc275cc371f1fcc87ef7
ValeriankaS <timonkin.max@yandex.com>
2025-10-01 12:28:36 +0300

    Описание функций secret-service с помощью jsdoc

---
+++ secret-backend/src/modules/secret/interfaces/create-secret.interface.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export interface CreateSecret {
+  readonly secretPhrase: string;
+  readonly expiresInTimestamp: number;
+  readonly availableViews: 1 | 3 | 5 | 10;
+  readonly allowDeletions: boolean;
+}
